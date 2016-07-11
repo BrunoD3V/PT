2d60fea91482457bc3edcb75694560cd0e3f0d89
pmpb75 <pedrobelchior75@gmail.com>
2016-07-11 17:17:10 +0100

    filepaths everywhere

---
+++ PTurismo/PTurismo/Models/FilePathCategoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace PTurismo.Models
+{
+    public class FilePathCategoria
+    {
+        [ForeignKey("Categoria")]
+        public int FilePathCategoriaID { get; set; }
+        [StringLength(255)]
+        public string FileName { get; set; }
+        public string Descricao { get; set; }
+        public FileType FileType { get; set; }
+        [JsonIgnore]
+        public virtual Categoria Categoria { get; set; }
+    }
+}
